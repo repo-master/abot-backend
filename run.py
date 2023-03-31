1a69b5ef310bcaa29b078482d10834ba9d1cf48b
Narayan Bandodker <repomaster@phaidelta.com>
2023-03-31 15:53:50 +0530

    Fix uvicorn runner factory flag

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 if __name__ == '__main__':
   # Start with reloading enabled (must be string to work)
-  uvicorn.run("abotcore:create_app", host='localhost', port=8000, reload=True)
+  uvicorn.run("abotcore:create_app", host='localhost', port=8000, factory=True, reload=True)
