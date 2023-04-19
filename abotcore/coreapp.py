018beb9ca55d4971432b11e142f2e717a2a2db4d
Narayan Bandodker <repomaster@phaidelta.com>
2023-04-19 10:50:04 +0530

    Unhandled Exception handling JSON response

--- abotcore/coreapp.py
+++ abotcore/coreapp.py
@@ -1,7 +1,9 @@
 
 import asyncio
+import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from contextlib import asynccontextmanager
@@ -28,7 +30,12 @@ async def app_lifespan(app: FastAPI):
 
 
 def create_app() -> FastAPI:
+    ### Application instance ###
     app = FastAPI(lifespan=app_lifespan)
+    logger = logging.getLogger(__name__)
+
+
+    ### Middleware ###
 
     # Enable cross-origin request, from any domain (*)
     app.add_middleware(
@@ -38,6 +45,20 @@ def create_app() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+
+    ### Exception handlers ###
+
+    @app.exception_handler(OSError)
+    async def unhandled_oserror_exception_handler(request: Request, exc: OSError):
+        logger.exception("Unhandled OSError while serving a request:", exc_info=exc)
+        return JSONResponse(
+            status_code=500,
+            content={"exception": 'OSError', "detail": str(exc)}
+        )
+
+
+    ### Routes ###
 
     # Add routes to the application
     app.include_router(chat.router)
