def54d2f93bdd8a3c8ea8685892584970faa1bcd
Ashpak-phaiDelta <a.shaikh@phaidelta.com>
2023-04-27 17:28:13 +0530

    done changes for report

--- abotcore/api_data/services.py
+++ abotcore/api_data/services.py
@@ -26,6 +26,12 @@ from .schemas import (
 
 from datetime import datetime, timedelta, timezone
 from typing import List, Tuple, Optional
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import io
+import base64
 
 
 class SensorDataService:
@@ -86,8 +92,52 @@ class SensorDataService:
 
 class GraphPlotService:
     async def plot_from_sensor_data(self, sensor_metadata: SensorMetadataOut, sensor_data: SensorDataOut) -> str:
-        with open("data/sample-graph.png", "rb") as img_file:
-            img_data64 = base64.b64encode(img_file.read()).decode('utf-8')
-            img_mimetype = 'image/png'
-            uri = "data:%s;base64,%s" % (img_mimetype, img_data64)
-            return uri
+        df = pd.DataFrame([s.__dict__ for s in sensor_data])
+        df['value'] = [x['value'] for x in df['value']]
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        graph_image = self.plot_graph(df, x_axis='timestamp', y_axis='value', x_label="Timestamp", y_label="Value",title=sensor_metadata.sensor_name)
+        return graph_image
+
+
+    def plot_graph(self, df, x_axis, y_axis, x_label=None, y_label=None, title=None):
+        # plot the data
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(df[x_axis], df[y_axis])
+        
+        # set the x-axis label and y-axis label
+        if x_label is not None:
+            ax.set_xlabel(x_label)
+
+        if y_label is not None:
+            ax.set_ylabel(y_label)
+        
+        # add title to the plot if provided
+        if title is not None:
+            ax.set_title(title)
+        
+        # format the tick labels on the x-axis
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+        for tick in ax.get_xticklabels():
+            tick.set_rotation(30)
+            
+        # adjust plot margins
+        fig.subplots_adjust(top=0.88, left=0.11, bottom=0.3, right=0.9)
+        
+        # save plot as png image to memory buffer
+        img_buffer = io.BytesIO()
+        fig.savefig(img_buffer, format='png') 
+        fig.savefig("my_plot.png")#Temp for reffrence
+        img_buffer.seek(0)
+        
+        # encode plot image buffer to base64 string
+        img_base64 = base64.b64encode(img_buffer.getvalue()).decode('utf-8')
+        
+        # clear the buffer
+        img_buffer.truncate(0)
+            
+        return img_base64
+        # with open("data/sample-graph.png", "rb") as img_file:
+        #     img_data64 = base64.b64encode(img_file.read()).decode('utf-8')
+        #     img_mimetype = 'image/png'
+        #     uri = "data:%s;base64,%s" % (img_mimetype, img_data64)
+        #     return uri
